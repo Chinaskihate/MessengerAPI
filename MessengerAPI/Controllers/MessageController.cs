215423af520c4fa0df0ae4baa8d5a35be3015dfb
Chinaskihate <islam.kadyrov.dlschool@gmail.com>
2021-06-10 19:57:23 +0300

    Доделан контроллер для сообщений.

--- MessengerAPI/Controllers/MessageController.cs
+++ MessengerAPI/Controllers/MessageController.cs
@@ -10,8 +10,15 @@ namespace MessengerAPI.Controllers
     /// </summary>
     public class MessageController : Controller
     {
+        #region Поля класса.
         private static List<Message> _messages = new List<Message>();
+        #endregion
 
+        /// <summary>
+        /// Отправление сообщения.
+        /// </summary>
+        /// <param name="req"> Запрос на отправку. </param>
+        /// <returns></returns>
         [HttpPost("/send-message")]
         public IActionResult SendMessage([FromBody] SendMessageRequest req)
         {
@@ -21,11 +28,41 @@ namespace MessengerAPI.Controllers
             return Ok(message);
         }
 
+        /// <summary>
+        /// Получение списка сообщений между двумя пользователями.
+        /// </summary>
+        /// <param name="senderId"> ID отправителя. </param>
+        /// <param name="receiverId"> ID получателя. </param>
+        /// <returns></returns>
         [HttpGet("/get-messages")]
         public IActionResult GetMessagesBySenderAndReceiver([FromQuery] int senderId, [FromQuery] int receiverId)
         {
             var result = _messages.Where(x => x.SenderId == senderId 
                                            && x.ReceiverId== receiverId);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Получение списка сообщений(по отправителю).
+        /// </summary>
+        /// <param name="senderId"> ID отправителя. </param>
+        /// <returns></returns>
+        [HttpGet("/get-messages-by-sender")]
+        public IActionResult GetMessagesBySender([FromQuery] int senderId)
+        {
+            var result = _messages.Where(x => x.SenderId == senderId);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Получение списка сообщений(по получателю).
+        /// </summary>
+        /// <param name="receiverId"> ID получателя. </param>
+        /// <returns></returns>
+        [HttpGet("/get-messages-by-receiver")]
+        public IActionResult GetMessagesByReceiver([FromQuery] int receiverId)
+        {
+            var result = _messages.Where(x => x.ReceiverId == receiverId);
             return Ok(result);
         }
     }

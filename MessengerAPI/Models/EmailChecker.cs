123781f62ae670092c2685ae70decef24662274d
Chinaskihate <islam.kadyrov.dlschool@gmail.com>
2021-06-10 18:35:13 +0300

    Создан класс для проверки почты.

---
+++ MessengerAPI/Models/EmailChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerAPI.Models
+{
+    internal static class EmailChecker
+    {
+        /// <summary>
+        /// Проверка почты на корректность.
+        /// </summary>
+        /// <param name="email"> Почта. </param>
+        /// <returns> Корректность почты. </returns>
+        internal static bool IsValidEmail(string email)
+        {
+            // TODO: оптимизировать.
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}

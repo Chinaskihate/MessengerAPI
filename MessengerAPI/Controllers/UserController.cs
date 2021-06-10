31fcce8a1dbdc8437a97b7f3381bbb1e076b6371
Chinaskihate <islam.kadyrov.dlschool@gmail.com>
2021-06-10 19:00:14 +0300

    Созданы модели контроллера для работы с пользователем(без описания), запроса на создание пользователя.

---
+++ MessengerAPI/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MessengerAPI.Models;
+
+namespace MessengerAPI.Controllers
+{
+    /// <summary>
+    /// Контроллер для работы с пользователем.
+    /// </summary>
+    public class UserController
+    {
+
+    }
+}
---
+++ MessengerAPI/Models/CreateUserRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerAPI.Models
+{
+    /// <summary>
+    /// Запрос на создание пользователя.
+    /// </summary>
+    public class CreateUserRequest
+    {
+        /// <summary>
+        /// Имя пользователя.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Почта пользователя.
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
